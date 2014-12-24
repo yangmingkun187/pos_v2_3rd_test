e41134f6458442e92622f42b509821bac5e6cbd8
mkyang <303055758@qq.com>
2014-12-24 19:52:57 +0800

    create test to cartItem.getPrommotionCount()

---
+++ pos_v2_3rdlib/__tests__/cartItem-test.js
@@ -0,0 +1,25 @@
+jest.dontMock('../src/model/cart_item');
+
+describe('CartItem', function() {
+  describe('#getPromotionCount', function() {
+    it('should return correct string', function() {
+      var CartItem = require('../src/model/cart_item');
+
+      item = {
+        barcode:'ITEM000001',
+        name : '雪碧',
+        unit : '瓶',
+        price : 3.00
+      };
+      var cartItem = new CartItem(item, 5);
+
+      cartItem.getPromotionType = jest.genMockFn();
+
+      cartItem.getPromotionType.mockReturnValue('BUY_TWO_GET_ONE_FREE');
+
+      var result = cartItem.getPromotionCount();
+
+      expect(result).toBe(1);
+    });
+  });
+});
